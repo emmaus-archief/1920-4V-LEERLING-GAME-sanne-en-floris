2128466ce1fb01cbba4885154f821b9a5ba34911
SanneV <sanneversl@gmail.com>
2020-04-06 15:25:03 +0000

    poging bewegen

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ var vijandY = 0;   // y-positie van vijand
 
 var score = 0; // aantal behaalde punten
 
+var x = 0;
+var y = 0;
+
 
 
 
@@ -78,9 +81,24 @@ var tekenKogel = function(x, y) {
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
+
+
 var tekenSpeler = function(x, y) {
   fill("white");
   ellipse(x, y, 50, 50);
+};
+
+  function  keyTyped() {
+
+  if (key === 'a') {
+    x = x - 1;
+  } else if (key === 'd') {
+    x = x + 1;
+  } else if (key=== 'w'){
+    y = y + 1;
+  } else if (key === 's'){
+    y = y - 1;
+  }
 };
 
 
