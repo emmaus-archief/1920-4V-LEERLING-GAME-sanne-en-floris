89468abfdea7ffb8e54a02c0cf094e6481e7db8c
SanneV <sanneversl@gmail.com>
2020-04-07 08:13:47 +0000

    poging bewegen 2

--- script.js
+++ script.js
@@ -36,10 +36,6 @@ var score = 0; // aantal behaalde punten
 var x = 0;
 var y = 0;
 
-
-
-
-
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
 /* ********************************************* */
@@ -77,30 +73,27 @@ var tekenKogel = function(x, y) {
 
 
 /**
- * Tekent de speler
+ * Tekent de speler1
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
 
-
-var tekenSpeler = function(x, y) {
+ function draw() {
   fill("white");
   ellipse(x, y, 50, 50);
-};
+}
 
-  function  keyTyped() {
-
-  if (key === 'a') {
+function keyPressed(){
+  if (keyCode === "a") {
     x = x - 1;
-  } else if (key === 'd') {
+  } else if (keyCode === 'd') {
     x = x + 1;
-  } else if (key=== 'w'){
+  } else if (keyCode === 'w'){
     y = y + 1;
-  } else if (key === 's'){
+  } else if (keyCode  === 's'){
     y = y - 1;
   }
-};
-
+}
 
 /**
  * Updatet globale variabelen met positie van vijand of tegenspeler
