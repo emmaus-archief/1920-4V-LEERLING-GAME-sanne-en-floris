85150dbc55398fa32989b6d6ea3b67d700f947b8
SanneV <sanneversl@gmail.com>
2020-05-20 09:36:16 +0000

    code netjes uitgelijnd

--- script.js
+++ script.js
@@ -41,70 +41,81 @@ var score = 0; // aantal behaalde punten
 /* ********************************************* */
 
 
-/**
+/*
  * Tekent het speelveld
  */
-var tekenVeld = function () {
+var tekenVeld = function ()
+{
   fill (78, 219, 255);
   rect (20, 20, width - 2 * 20, height - 2 * 20);
   
-  if(xSpeler1>1210){
-  xSpeler1=1210;
-  };
-  if(xSpeler1<100){
-      xSpeler1=100;
-  };
-  if(xSpeler2>1180){
-  xSpeler2=1180;
-  };
-  if(xSpeler2<70){
-      xSpeler2=70;
-  };
-  if(ySpeler1<50){
-      ySpeler1=50;
-  };
-  if(ySpeler1>670){
-      ySpeler1=670;
-  };
-  if(ySpeler2<50){
-      ySpeler2=50;
-  };
-  if(ySpeler2>670){
-      ySpeler2=670;
-  };
-};
+  if(xSpeler1 > 1210)
+  {
+    xSpeler1 = 1210;
+  }
+  if(xSpeler1 < 100)
+  {
+    xSpeler1 = 100;
+  }
+  if(xSpeler2 > 1180)
+  {
+    xSpeler2 = 1180;
+  }
+  if(xSpeler2 < 70)
+  {
+    xSpeler2 = 70;
+  }
+  if(ySpeler1 < 50)
+  {
+    ySpeler1 = 50;
+  }
+  if(ySpeler1 > 670)
+  {
+    ySpeler1 = 670;
+  }
+  if(ySpeler2 < 50)
+  {
+    ySpeler2 = 50;
+  }
+  if(ySpeler2 > 670)
+  {
+    ySpeler2 = 670;
+  }
+}
 
 
-
-/**
+/*
  * Tekent de vijand
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
-var tekenVijand = function(x, y) {
+var tekenVijand = function(x, y)
+{
     
 
-};
+}
 
 
-/**
+/*
  * Tekent de kogel of de bal
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
-var tekenKogel = function(x, y) {
+var tekenKogel = function(x, y)
+{
 
 
-};
+}
 
 
-/**
+/*
  * Tekent de speler1
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
 
- var tekenSpeler1 = function(){
+ var tekenSpeler1 = function()
+ {
     fill("blue");
     triangle(xSpeler1-60, ySpeler1+25, xSpeler1-60, ySpeler1-25, xSpeler1-20, ySpeler1); // Staart
     fill("blue");
@@ -113,17 +124,17 @@ var tekenKogel = function(x, y) {
     ellipse(xSpeler1+10, ySpeler1-10, 15, 15); // Oog
     fill("black");
     ellipse(xSpeler1+10, ySpeler1-10, 7, 7); // Pupil
- };
+ }
 
 
-    /**
-     * Tekent de speler2
-     * @param {number} x x-coördinaat
-     * @param {number} y y-coördinaat
-     */
+/*
+ * Tekent de speler2
+ * @param {number} x x-coördinaat
+ * @param {number} y y-coördinaat
+*/
 
-     var tekenSpeler2 = function(){
-    
+var tekenSpeler2 = function()
+{ 
     fill("red");
     triangle(xSpeler2+60, ySpeler2+25, xSpeler2+60, ySpeler2-25, xSpeler2+20, ySpeler2); // Staart
     fill("red");
@@ -132,103 +143,116 @@ var tekenKogel = function(x, y) {
     ellipse(xSpeler2-10, ySpeler2-10, 15, 15); //Oog
     fill("black");
     ellipse(xSpeler2-10, ySpeler2-10, 7, 7); // Pupil
+}
 
-     };
-
-
-/**
+/*
  * Updatet globale variabelen met positie van vijand of tegenspeler
  */
-var beweegVijand = function() {
+var beweegVijand = function()
+{
     
-};
+}
 
 
-/**
+/*
  * Updatet globale variabelen met positie van kogel of bal
  */
-var beweegKogel = function() {
+var beweegKogel = function()
+{
 
-};
+}
 
 
-/**
+/*
  * Kijkt wat de toetsen/muis etc zijn.
  * Updatet globale variabele spelerX en spelerY
  * 
  */
-var beweegSpeler1 = function() {
+var beweegSpeler1 = function()
+{
      
-    if(keyIsDown(68)){
+    if(keyIsDown(68))
+    {
         xSpeler1=xSpeler1+2; // Toets D
-    };
-    if(keyIsDown(65)){
+    }
+    if(keyIsDown(65))
+    {
         xSpeler1=xSpeler1-2; // Toets A
-    };
-    if(keyIsDown(83)){
+    }
+    if(keyIsDown(83))
+    {
         ySpeler1=ySpeler1+2; // Toets W
-    };
-    if(keyIsDown(87)){
+    }
+    if(keyIsDown(87))
+    {
         ySpeler1=ySpeler1-2; // Toets S
-    };
+    }
 
-};
+}
 
 var beweegSpeler2 = function(){
     
-    if(keyIsDown(76)){
+    if(keyIsDown(76))
+    {
         xSpeler2=xSpeler2+2; // Toets L
-    };
-    if(keyIsDown(74)){
+    }
+    if(keyIsDown(74))
+    {
         xSpeler2=xSpeler2-2; // Toets J
-    };
-    if(keyIsDown(75)){
+    }
+    if(keyIsDown(75))
+    {
         ySpeler2=ySpeler2+2; // Toets I
-    };
-    if(keyIsDown(73)){
+    }
+    if(keyIsDown(73))
+    {
         ySpeler2=ySpeler2-2; // Toets K
-    };
+    }
 
-};
+}
 
 
-/**
+/*
  * Zoekt uit of de vijand is geraakt
  * @returns {boolean} true als vijand is geraakt
  */
-var checkVijandGeraakt = function() {
+var checkVijandGeraakt = function()
+{
 
   return false;
-};
+}
 
 
-/**
+/*
  * Zoekt uit of de speler is geraakt
  * bijvoorbeeld door botsing met vijand
  * @returns {boolean} true als speler is geraakt
  */
-var checkSpelerGeraakt = function() {
+var checkSpelerGeraakt = function()
+{
     
   return false;
-};
+}
 
 
-/**
+/*
  * Zoekt uit of het spel is afgelopen
  * @returns {boolean} true als het spel is afgelopen
  */
-var checkGameOver = function() {
+var checkGameOver = function()
+{
     
   return false;
-};
+}
 
 
-/**
+/*
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
-function setup() {
+function setup()
+{
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
@@ -237,12 +261,13 @@ function setup() {
 }
 
 
-/**
+/*
  * draw
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
-function draw() {
+function draw()
+{
   switch (spelStatus) {
     case SPELEN:
       beweegVijand();
@@ -266,7 +291,8 @@ function draw() {
       tekenSpeler1();
       tekenSpeler2();
 
-      if (checkGameOver()) {
+      if (checkGameOver()) 
+      {
         spelStatus = GAMEOVER;
       }
       break;
